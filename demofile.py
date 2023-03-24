0e9bb4a9f7f7e336a9e59e944d9282585f76faed
Srijan Tripathi <srijantripathi01@gmail.com>
2023-03-24 21:57:46 +0530

    Revert "making a commit which is this time right ,for the purpose of git revert"
    This reverts commit 8571b0839bb76915992f5609a4a0d8e059c5b873.

--- demofile.py
+++ demofile.py
@@ -1,4 +1,3 @@
 print("Getting started with GNU Nano text editor ")
 print("Second line is removed and now there is only two lines")
 
-print("Making changes so that I can use revert to change commit message ")

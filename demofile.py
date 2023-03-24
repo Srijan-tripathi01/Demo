046a69f862c6ddc6d9622c20e05bd49c30647d03
Srijan Tripathi <srijantripathi01@gmail.com>
2023-03-24 22:08:56 +0530

    Adding Modified file in staging area and commiting simultaneously(in one go)

--- demofile.py
+++ demofile.py
@@ -1,3 +1,4 @@
 print("Getting started with GNU Nano text editor ")
 print("Second line is removed and now there is only two lines")
 
+print("Adding modified file in staging ared and commiting simultaneously")

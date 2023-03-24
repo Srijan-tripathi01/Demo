8571b0839bb76915992f5609a4a0d8e059c5b873
Srijan Tripathi <srijantripathi01@gmail.com>
2023-03-24 21:56:31 +0530

    making a commit which is wrong ,for the purpose of git revert

--- demofile.py
+++ demofile.py
@@ -1,3 +1,4 @@
 print("Getting started with GNU Nano text editor ")
 print("Second line is removed and now there is only two lines")
 
+print("Making changes so that I can use revert to change commit message ")

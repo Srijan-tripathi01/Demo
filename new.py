52353cf664413fdbe2880e7c5e68489e34d1c6a6
Srijan Tripathi <srijantripathi01@gmail.com>
2023-03-25 22:51:03 +0530

    Adding a new line to new.py

--- new.py
+++ new.py
@@ -1,4 +1,5 @@
 print("Created new file ,so that it can be pushed to local repo")
 
 print("Created this line for new branch for first time")
- print("How to push the branch work to github repo")
+print("How to push the branch work to github repo")
+print("returning to main branch")
